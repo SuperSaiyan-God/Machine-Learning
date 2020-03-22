a06576c0b678273138d566d469ed608595530ef7
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2020-03-22 13:57:31 +0530

    new

---
+++ Support Vector Machine/supportvm.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+#Replace missing data
+df.replace('?', -99999, inplace=True)
+#Remove useless data
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
